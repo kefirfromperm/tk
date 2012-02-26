6052fe13686bee8f1bdcbd294b435ebac9f38443
Vitalii Samolovskikh <kefir@perm.ru>
2012-02-26 18:04:04 +0600

    Configuration holder

--- tk/src/org/kefirsf/tk/RenderServlet.java
+++ tk/src/org/kefirsf/tk/RenderServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 
 /**
