8984814ecf8ddc21a2a7b125c0c56c62ee261b20
Vitalii Samolovskikh <kefir@perm.ru>
2012-02-26 20:57:00 +0600

    Ping servlet

---
+++ tk/src/org/kefirsf/tk/PingServlet.java
@@ -0,0 +1,22 @@
+package org.kefirsf.tk;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet for ping application.
+ *
+ * @author Vitalii Samolovskikh aka Kefir
+ */
+public class PingServlet extends HttpServlet {
+    protected void service(
+            HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write("I'm OK!");
+        response.flushBuffer();
+    }
+}
