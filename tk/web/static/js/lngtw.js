7e946d3541828d558e60c64f1697884bcb4bece1
Vitalii Samolovskikh <kefir@perm.ru>
2012-04-12 13:42:18 +0600

    The follow twitter button. Flags for change locale.

--- tk/web/static/js/lngtw.js
+++ tk/web/static/js/lngtw.js
@@ -12,6 +12,9 @@ $(document).ready(function () {
     messageEventListener();
     $('#message').change(messageEventListener);
     $('#message').keyup(messageEventListener);
+
+    // Init twitter widget
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 });
 
 function messageEventListener() {
