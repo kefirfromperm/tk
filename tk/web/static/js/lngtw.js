e4f9a1abaa1111424025baea34831aeb94062996
Vitalii Samolovskikh <kefir@perm.ru>
2012-07-17 22:33:33 +0600

    Resolved max message length in JavaScript

--- tk/web/static/js/lngtw.js
+++ tk/web/static/js/lngtw.js
@@ -39,7 +39,7 @@ function messageEventListener() {
     if ($('#message').size() > 0 && $('#counter').empty().size() > 0) {
         var length = $('#message').val().length;
         $('#counter').text(length);
-        if (length > 2000) {
+        if (length > 1300) {
             $('#counter').css('color', 'red');
         } else if (length > 140) {
             $('#counter').css('color', 'green');
