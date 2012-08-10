0c3ebd649dd980d05d8d4838202314aacad95405
Vitalii Samolovskikh <kefir@perm.ru>
2012-08-10 21:10:53 +0600

    Redirect from www.lngtw.com to lngtw.com

--- tk/src/org/kefirsf/tk/WwwFilter.java
+++ tk/src/org/kefirsf/tk/WwwFilter.java
@@ -28,9 +28,6 @@ public class WwwFilter implements Filter {
             serverName = serverName.substring(WWW_PREFIX.length());
             int serverPort = request.getServerPort();        // 80
             String contextPath = request.getContextPath();   // /mywebapp
-            String servletPath = request.getServletPath();   // /servlet/MyServlet
-            String pathInfo = request.getPathInfo();         // /a/b;c=123
-            String queryString = request.getQueryString();          // d=789
 
             // Reconstruct original requesting URL
             StringBuilder url =  new StringBuilder();
@@ -40,16 +37,12 @@ public class WwwFilter implements Filter {
                 url.append(":").append(serverPort);
             }
 
-            url.append(contextPath).append(servletPath);
-
-            if (pathInfo != null) {
-                url.append(pathInfo);
-            }
-            if (queryString != null) {
-                url.append("?").append(queryString);
+            if(contextPath!=null && contextPath.length()>0){
+                url.append(contextPath).append("/");
             }
 
-            response.sendRedirect(url.toString());
+            response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+            response.setHeader("Location", url.toString());
         } else {
             chain.doFilter(req, resp);
         }
